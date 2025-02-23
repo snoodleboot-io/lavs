225e85ffe8177f7166bd4fb6ec47ecd4041c7c6c
John Aven <john.aven.phd@gmail.com>
2025-02-23 10:34:04 -0600

    feat: Refactored for annotation and more integrated pydantic usage

--- app/routers/patch.py
+++ app/routers/patch.py
@@ -3,6 +3,8 @@ from typing import Annotated
 from fastapi import APIRouter, Query
 
 from app.models.requests.application_name_model import ApplicationNameModel
+from app.models.respones.applciation_and_version_response_model import ApplicationAndVersionResponseModel
+from app.models.respones.patch_response_model import PatchResponseModel
 from app.queries.patch_version.create_patch import CreatePatch
 from app.queries.patch_version.read_current_patch import ReadCurrentPatch
 from app.queries.patch_version.rollback_to_previous_patch_version import (
@@ -12,24 +14,24 @@ from app.queries.patch_version.rollback_to_previous_patch_version import (
 router = APIRouter(tags=["patch"], prefix="/patch")
 
 
-@router.post("/")
+@router.post("/", response_model=ApplicationAndVersionResponseModel)
 async def create(data: Annotated[ApplicationNameModel, Query()]):
     """Create the next patch version."""
     result = await CreatePatch().execute(data=data)
 
-    return {"result": result}
+    return result
 
 
-@router.get("/")
+@router.get("/", response_model=PatchResponseModel)
 async def get(data: Annotated[ApplicationNameModel, Query()]):
-    """Retrieve the next patch version."""
+    """Retrieve the current patch version."""
     result = await ReadCurrentPatch().execute(data=data)
 
     return {"result": result}
 
 
-@router.post("/rollback")
+@router.post("/rollback", response_model=ApplicationAndVersionResponseModel)
 async def rollback(data: Annotated[ApplicationNameModel, Query()]):
     result = await RollbackToPreviousPatchVersion().execute(data=data)
 
-    return {"result": result}
+    return result
